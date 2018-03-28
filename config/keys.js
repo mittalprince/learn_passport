09673922023226735f43f584f038a9d906387690
mittalprince <princemittal766@gmail.com>
2018-03-28 19:59:32 +0530

    update gitignore

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google:{
+        clientID : '572518517827-slif61i17l0s0ptptb5btprp7ekq65up.apps.googleusercontent.com',
+        clientSecret : '4Zs5EbgRu46qOiN0DgjT1LKj'
+    },
+    mongodb:{
+        dbURL : 'mongodb://mittalprince:prince25@ds123499.mlab.com:23499/socialauthentication'
+    },
+    session:{
+        cookieKey: 'miitalprinceisawesome'
+    }
+};
