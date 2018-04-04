75f7991d49d40f08cc94bd21bd3aa7aeaa3634de
mittalprince <princemittal766@gmail.com>
2018-04-04 22:47:08 +0530

    remove silly mistakes

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,7 @@ module.exports = {
         clientSecret:process.env.clientSecret
     },
     mongodb: {
-        dbURI: `mongodb://${process.env.username}:${password}@ds235169.mlab.com:35169/social-authentication`
+        dbURI: `mongodb://${process.env.username}:${process.env.password}@ds235169.mlab.com:35169/social-authentication`
     },
     session: {
         cookieKey: 'mittalprinceawesome'
