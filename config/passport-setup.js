a40ba7bf69255bd46d824ebc73338392e250967f
mittalprince <princemittal766@gmail.com>
2018-04-05 10:38:35 +0530

    make it better

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,4 +1,4 @@
-const passport = require('assport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
 const User = require('../models/user-model');
